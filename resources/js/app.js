84ab51ca475b345a35a461270e7bb0685ad3ae06
amaralfatah <amaralfatah.main@gmail.com>
2025-02-13 11:01:34 +0700

    fix: app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,12 @@
 // resources/js/app.js
 
 import './bootstrap';
-import jQuery from 'jquery';
+import $ from 'jquery';  // Mengimpor jQuery
 import DataTable from 'datatables.net-bs5';
 import 'datatables.net-bs5/css/dataTables.bootstrap5.css';
 
-// Set jQuery globally
-window.$ = window.jQuery = jQuery;
-
-// Initialize DataTables
-window.DataTable = DataTable(window, $);
-
+// Tidak perlu menulis ke `window`, jQuery dan DataTable sudah dapat digunakan langsung
+$(document).ready(function() {
+    // Inisialisasi DataTable
+    $('.datatable').DataTable(); // Sesuaikan dengan selector Anda
+});
