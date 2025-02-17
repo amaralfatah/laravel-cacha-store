34e576e4176a98f7ecba980b7b425efe4a1629dc
amaralfatah <amaralfatah.main@gmail.com>
2025-02-17 16:59:06 +0700

    fix: variable undefined

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -378,12 +378,12 @@
                         @foreach ($recentTransactions as $transaction)
                             <li class="d-flex mb-4 pb-1">
                                 <div class="avatar flex-shrink-0 me-3">
-                                    <img src="{{ asset('sneat/assets/img/icons/unicons/' . ($transaction['type'] === 'cash' ? 'wallet.png' : 'cc-primary.png')) }}"
+                                    <img src="{{ asset('sneat/assets/img/icons/unicons/' . ($transaction['payment_type'] === 'cash' ? 'wallet.png' : 'cc-primary.png')) }}"
                                         alt="User" class="rounded">
                                 </div>
                                 <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                                     <div class="me-2">
-                                        <small class="text-muted d-block mb-1">{{ ucfirst($transaction['type']) }}</small>
+                                        <small class="text-muted d-block mb-1">{{ ucfirst($transaction['payment_type']) }}</small>
                                         <h6 class="mb-0">{{ $transaction['customer'] }}</h6>
                                     </div>
                                     <div class="user-progress d-flex align-items-center gap-1">
