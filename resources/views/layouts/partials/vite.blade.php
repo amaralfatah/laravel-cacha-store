cf9459503f50fc736afd0f17a92c6feb38e25965
Amar Al Fatah <amaralfatah.me@gmail.com>
2025-02-21 21:32:07 +0700

    fix: vite

--- resources/views/layouts/partials/vite.blade.php
+++ resources/views/layouts/partials/vite.blade.php
@@ -1,17 +1,17 @@
 @php
     $isProduction = app()->environment('production');
-    $manifestPath = $isProduction ? '../public_html/build/manifest.json' : public_path('build/manifest.json');
+    $manifestPath = public_path('build/manifest.json');
 @endphp
 
-@if ($isProduction && file_exists($manifestPath))
+@if (file_exists($manifestPath))
     @php
         $manifest = json_decode(file_get_contents($manifestPath), true);
     @endphp
-    <link rel="stylesheet" href="{{ config('app.url') }}/build/{{ $manifest['resources/css/app.css']['file'] }}">
-    <script type="module" src="{{ config('app.url') }}/build/{{ $manifest['resources/js/app.js']['file'] }}"></script>
+    <link rel="stylesheet" href="{{ asset('build/'.$manifest['resources/css/app.css']['file']) }}">
+    <script type="module" src="{{ asset('build/'.$manifest['resources/js/app.js']['file']) }}"></script>
     @if (isset($manifest['resources/js/app.js']['css']))
         @foreach ($manifest['resources/js/app.js']['css'] as $css)
-            <link rel="stylesheet" href="{{ config('app.url') }}/build/{{ $css }}">
+            <link rel="stylesheet" href="{{ asset('build/'.$css) }}">
         @endforeach
     @endif
 @else
